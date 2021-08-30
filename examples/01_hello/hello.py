7514f1ce7d9714eb78004dcdebf5f33a666eec66
Bradley <bschlottman@email.arizona.edu>
2021-08-30 10:17:07 -0700

    changed tabs to 4spaces

--- examples/01_hello/hello.py
+++ examples/01_hello/hello.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #Author: Bradley Schlottman, taken from Ken Yoens-Clark <bschlottman@email.arizona.edu>
 # Purpose: Say hello
@@ -7,17 +7,17 @@
 import argparse
 
 def get_args():
-	parser = argparse.ArgumentParser(description='Say Hello')
-	parser.add_argument('-n', '--name', metavar='name',
-					default='World', help='Name to greet')
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name',
+					default='Friend', help='Name to greet')
+    return parser.parse_args()
 	
 def main():
-	args = get_args()
-	print('Hello, ' + args.name + '!')
+    args = get_args()
+    print('Hello, ' + args.name + '!')
 	
 if __name__ == '__main__':
-	main()
+    main()
 	
 
 
