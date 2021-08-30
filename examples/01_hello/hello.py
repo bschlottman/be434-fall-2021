2cfb240b0e68dfaf3eaebc3cc4c0de5079852e29
Bradley <lillymoore@dhcp-10-142-237-125.uawifi.arizona.edu>
2021-08-30 09:06:12 -0700

    added main() function

--- examples/01_hello/hello.py
+++ examples/01_hello/hello.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python3
-"""
-Author:  Ken Youens-Clark <kyclark@gmail.com>
-Purpose: Say hello
-"""
+#!/usr/bin/env python
+
+#Author: Bradley Schlottman, taken from Ken Yoens-Clark <bschlottman@email.arizona.edu>
+# Purpose: Say hello
+
 
 import argparse
 
-
-# --------------------------------------------------
 def get_args():
-    """Get the command-line arguments"""
-
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', default='World', help='Name to greet')
-    return parser.parse_args()
-
-
-# --------------------------------------------------
+	parser = argparse.ArgumentParser(description='Say Hello')
+	parser.add_argument('-n', '--name', metavar='name',
+					default='World', help='Name to greet')
+	return parser.parse_args()
+	
 def main():
-    """Make a jazz noise here"""
+	args = get_args()
+	print('Hello, ' + args.name + '!')
+	
+if _name_ == '_main_':
+	main()
+	
 
-    args = get_args()
-    print('Hello, ' + args.name + '!')
 
-
-# --------------------------------------------------
-if __name__ == '__main__':
-    main()
