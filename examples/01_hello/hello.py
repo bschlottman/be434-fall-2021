cda26e679b6038f3c9edbc4cfe92cacd2d85221f
Bradley <bschlottman@email.arizona.edu>
2021-09-01 10:20:02 -0700

    stashing changes before merging

--- examples/01_hello/hello.py
+++ examples/01_hello/hello.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-#Author: Bradley Schlottman, taken from Ken Yoens-Clark <bschlottman@email.arizona.edu>
-# Purpose: Say hello
-
+"""
+Author: Bradley Schlottman, taken from Ken Yoens-Clark <bschlottman@email.arizona.edu>
+Purpose: Say hello
+"""
 
 import argparse
 
@@ -12,9 +13,11 @@ def get_args():
 					default='Friend', help='Name to greet')
     return parser.parse_args()
 	
+	
 def main():
     args = get_args()
     print('Hello, ' + args.name + '!')
+	
 	
 if __name__ == '__main__':
     main()
