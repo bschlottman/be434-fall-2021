96b4dbc2cf587e91abecc3bd5f28ea5831482d10
Bradley <bschlottman@email.arizona.edu>
2021-10-11 09:24:14 -0700

    apples.py solution 1 worked out

--- examples/08_apples_and_bananas/apples.py
+++ examples/08_apples_and_bananas/apples.py
@@ -2,10 +2,11 @@
 """
 Author : bschlottman <bschlottman@email.arizona.edu>
 Date   : 2021-10-10
-Purpose: Rock the Casbah
+Purpose: convert vowels in a string or text file to a provided vowel
 """
 
 import argparse
+import os
 
 
 # --------------------------------------------------
@@ -13,21 +14,26 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Apples and bananas',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument('text', metavar='str', help='Input text or file')
+    # Take a positional argument that might be some plain text
+    parser.add_argument(
+        '-v',
+        '--vowel',
+        help='The vowel to substitute',
+        choices=['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'],
+        metavar='str',
+        type=str,
+        default='a')
+    # or may name a file
+    args = parser.parse_args()
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
 
-    return parser.parse_args()
+    return args
 
 
 # --------------------------------------------------
@@ -35,8 +41,21 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+    text = args.text
+    vowel = args.vowel
+    vowel_dict_low = list('aeiou')
+    vowel_dict_up = vowel_dict_low.upper()
+    new_text = []
 
-    print(args)
+    for char in text:
+        if char in vowel_dict_low:
+            new_text.append(vowel)
+        elif char in vowel_dict_up:
+            new_text.append(vowel.upper())
+        else:
+            new_text.append(char)
+
+    print(''.join(new_text))
 
 
 # --------------------------------------------------
