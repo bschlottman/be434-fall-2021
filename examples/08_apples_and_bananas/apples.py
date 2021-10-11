6da57a940bd8268be8fcb2ac4f72bc69b9f916b9
Bradley <bschlottman@email.arizona.edu>
2021-10-10 19:14:31 -0700

    created new file

---
+++ examples/08_apples_and_bananas/apples.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Author : bschlottman <bschlottman@email.arizona.edu>
+Date   : 2021-10-10
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('positional',
+                        metavar='str',
+                        help='A positional argument')
+
+    parser.add_argument('-a',
+                        '--arg',
+                        help='A named string argument',
+                        metavar='str',
+                        type=str,
+                        default='')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    print(args)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
