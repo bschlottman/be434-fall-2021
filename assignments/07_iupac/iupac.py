d328766dca70a84b5282b212d01615dcfdfc6452
Bradley <bschlottman@email.arizona.edu>
2021-10-15 09:43:19 -0700

    help message complete for iupac.py

--- assignments/07_iupac/iupac.py
+++ assignments/07_iupac/iupac.py
@@ -1,50 +1,33 @@
 #!/usr/bin/env python3
 """
-Author : lillymoore <lillymoore@localhost>
+Author : bschlottman <bschlottman@email.arizona.edu>
 Date   : 2021-10-15
-Purpose: Rock the Casbah
+Purpose: Rock the Rialto
 """
 
 import argparse
-
+import re
+import sys
 
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Expand IUPAC codes',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
+    parser.add_argument('seq',
+                        metavar='SEQ',
+                        help='Input sequence(s)')
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outfile',
+                        help='Output file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
 
     return parser.parse_args()
 
@@ -54,17 +37,8 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    
+    print(args)
 
 
 # --------------------------------------------------
