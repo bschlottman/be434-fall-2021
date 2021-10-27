27d68c18c49ba75a37275175bba58105687d6361
Bradley <bschlottman@email.arizona.edu>
2021-10-27 09:14:57 -0700

    failing bad-kmer test

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -18,21 +18,27 @@ def get_args():
 
     parser.add_argument('FILE1',
                         metavar='FILE1',
-                        help='input Files to compare')
+                        help='input Files to compare',
+                        type=argparse.FileType('rt'))
 
     parser.add_argument('FILE2',
                         metavar='FILE2',
-                        help='input Files to compare')
+                        help='input Files to compare',
+                        type=argparse.FileType('rt'))
 
     parser.add_argument('-k',
-                        '--kmer',
+                        '--kmers',
                         help='K-mer size',
                         metavar='int',
                         type=int,
                         default='3')
 
+    args = parser.parse_args()
 
-    return parser.parse_args()
+    if args.kmers < 1:
+        parser.error(f'--kmers "{args.kmers}" must be > 0')
+
+    return args
 
 
 # --------------------------------------------------
@@ -40,7 +46,43 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(args)
+    F1 = args.FILE1
+    F2 = args.FILE2
+    kmers = args.kmers
+
+    k1 = count_kmers(F1, kmers)
+    k2 = count_kmers(F2, kmers)
+
+    commonk = sorted(list(set(k1).intersection(k2)))
+
+    for kmers in commonk:
+        print('{:10} {:6} {:6}'.format(kmers, k1.get(kmers), k2.get(kmers)),
+              end = '\n')
+
+
+# --------------------------------------------------
+def count_kmers(file, k):
+    """Create a dictionary of kmers in each file"""
+    
+    mers = {}
+    for line in file:
+        for word in line.split():
+            for kmers in find_kmers(word, k):
+                if kmers in mers:
+                    words[kmers] += 1
+                else:
+                    words[kmers] = 1
+    return mers
+
+
+# --------------------------------------------------
+def find_kmers(seq, k):
+    """Find kmers in a string"""
+
+    n = len(seq) - k + 1
+
+    return [] if n < 1 else [seq[i:i + k] for i in range(n)]
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
