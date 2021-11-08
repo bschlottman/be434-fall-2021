bac8edb035e36525d485e694f16bf26315d2063f
Bradley <bschlottman@email.arizona.edu>
2021-11-07 20:33:48 -0700

    added HW assignment 10 template

---
+++ assignments/10_conserved/conserved.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Author : lillymoore <lillymoore@localhost>
+Date   : 2021-11-07
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Find conserved bases',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('bases',
+                        metavar='FILE',
+                        nargs=1,
+                        type=argparse.FileType('rt'),
+                        help='Input file')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a ski noise here"""
+
+    args = get_args()
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
+
+    print(f'str_arg = "{str_arg}"')
+    print(f'int_arg = "{int_arg}"')
+    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
+    print(f'flag_arg = "{flag_arg}"')
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
