c89ccc7a09fe9f48b975cf04ef345f2121fab834
Bradley <bschlottman@email.arizona.edu>
2021-11-09 12:10:53 -0700

    10000000% passed conserved.py

--- assignments/10_conserved/conserved.py
+++ assignments/10_conserved/conserved.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : lillymoore <lillymoore@localhost>
+Author : bschlottman <bschlottman@email.arizona.edu>
 Date   : 2021-11-07
-Purpose: Rock the Casbah
+Purpose: Find the conserved bases in two or more aligned sequences
 """
 
 import argparse
@@ -16,13 +16,28 @@ def get_args():
         description='Find conserved bases',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('bases',
+    parser.add_argument('alignments',
                         metavar='FILE',
-                        nargs=1,
                         type=argparse.FileType('rt'),
-                        help='Input file')
+                        help='Input file',
+                        default=None)
 
     return parser.parse_args()
+
+
+# --------------------------------------------------
+def compare_chars(lines, line_num, position):
+    """"Compare each character in the aligned sequences"""
+
+    same_letters = {}
+    for letter in range(position):
+        compare = [lines[line][letter] for line in range(line_num)]
+        if len(set(compare)) != 1:
+            same_letters[letter] = 'X'
+        else:
+            same_letters[letter] = '|'
+
+    return same_letters
 
 
 # --------------------------------------------------
@@ -30,13 +45,18 @@ def main():
     """Make a ski noise here"""
 
     args = get_args()
-   
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    line_list = [line.rstrip() for line in args.alignments]
+    line_num = len(line_list)
+    position = len(line_list[0])
+
+    pos_match = compare_chars(line_list, line_num, position)
+
+    for lines in line_list:
+        print(lines.rstrip())
+
+    for pos in range(position):
+        print(pos_match[pos], end='')
 
 
 # --------------------------------------------------
