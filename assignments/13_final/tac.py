0b0179ececf66f613b5f5399e9e3abc17c675410
Bradley <bschlottman@email.arizona.edu>
2021-12-01 16:01:44 -0700

    updated tac.py, accepts good input, rejects nonexistent or unreadable input files

--- assignments/13_final/tac.py
+++ assignments/13_final/tac.py
@@ -6,6 +6,7 @@ Purpose: finish BE534
 """
 
 import argparse
+import sys
 
 
 # --------------------------------------------------
@@ -13,58 +14,34 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Python clone of tac',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
+    parser.add_argument('files',
+                        help='Input file(s)',
                         metavar='FILE',
+                        nargs='+',
                         type=argparse.FileType('rt'),
                         default=None)
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outfile',
+                        help='Output file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+    args = parser.parse_args()
 
-    return parser.parse_args()
-
+    if args.files != argparse.FileType('rt'):
+        parser.error(f"can't open {args.files}")
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(args)
 
 
 # --------------------------------------------------
